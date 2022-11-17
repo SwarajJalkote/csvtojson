b870f4bc96241bc4ebc7f20c4a85006131e88912
Swaraj Jalkote <swarajjalkote98@gmail.com>
2022-11-17 23:41:20 +0530

    Test cases complete

--- tests/test_csvtojson.py
+++ tests/test_csvtojson.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+import json
 from src.csvtojsonlib import read_csv_file, csv_to_json
 
 def test_read_csv_file():
@@ -19,6 +20,22 @@ def test_read_csv_file():
 
 
 def test_csv_to_json():
+    assert_json = [
+    {
+        "Rank in India": 1,
+        "Changes Rank from last Year": 0,
+        "Forbes 2000 rank in World": 54,
+        "Name": "Reliance Industries",
+        "Headquarters": "Mumbai"
+    },
+    {
+        "Rank in India": 2,
+        "Changes Rank from last Year": 0,
+        "Forbes 2000 rank in World": 105,
+        "Name": "State Bank of India",
+        "Headquarters": "Mumbai"
+    }
+    ]
     x = csv_to_json("input_file.csv", ",", column_names=[])
-
-    print(x)
+    x = json.loads(x)
+    assert assert_json == x
